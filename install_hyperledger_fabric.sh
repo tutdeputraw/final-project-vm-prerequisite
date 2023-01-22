8805876f5386ed4b24906a21c149f1c3ea329fac
tutdeputraw <anoboy.code@gmail.com>
2023-01-22 20:03:19 +0700

    adjust the .bashrc file

--- install_hyperledger_fabric.sh
+++ install_hyperledger_fabric.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.0.0 1.4.7
+
+if ! grep -q 'export PATH=$PATH:$GOPATH/bin:/home/ubuntu/fabric-samples/bin'; then
+	echo 'export PATH=$PATH:$GOPATH/bin:/home/ubuntu/fabric-samples/bin' >> ~/.bashrc
+fi
