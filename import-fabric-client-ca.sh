ad6fafa993ff2c07a550fda766d38a3319a6c45d
tutdeputraw <anoboy.code@gmail.com>
2023-01-22 20:29:14 +0700

    add init statement

--- import-fabric-client-ca.sh
+++ import-fabric-client-ca.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 if ! grep -q 'export PATH=$PATH:$GOPATH/bin:/home/ubuntu/fabric-samples/bin'; then
         echo 'export PATH=$PATH:$GOPATH/bin:/home/ubuntu/fabric-samples/bin' >> ~/.bashrc
         source ~/.bashrc
